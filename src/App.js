4766a806dc78f108c917d72d7cd0f69bcf56272f
itaweb <admod32@gmail.com>
2021-01-26 00:17:02 +0300

    Add photo on the index page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-// function Food({name}){
-//   return(<h3>Я люблю кушать {name}!</h3>);
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h3>Я люблю кушать {name}!</h3>
+      <img src={picture} />
+    </div>
+  );
+}
+
+// function Food(props) {
+//   return <h3>Я люблю кушать {props.name}!</h3>;
 // }
 
-function Food(props){
-  return(<h3>Я люблю кушать {props.name}!</h3>);
-}
+const foodILike = [
+  {
+    name: "борщ",
+    image:
+      "https://the-challenger.ru/wp-content/uploads/2019/04/shutterstock_570014164-800x534.jpg",
+  },
+  {
+    name: "драники",
+    image:
+      "https://2d-recept.com/wp-content/uploads/2019/05/draniki-kartofelnye-foto.jpg",
+  },
+  {
+    name: "стейк",
+    image:
+      "https://eda.ru/img/eda/c620x415i/s2.eda.ru/StaticContent/Photos/120213181923/120626165816/p_O.jpg",
+  },
+  {
+    name: "хинкали",
+    image: "https://foodman.club/wp-content/uploads/2017/12/Khinkali.jpg",
+  },
+];
 
 function App() {
   return (
     <div>
-      Привет!!!
-      <Food name='борщ' />
-      <Food name='картошку' />
-      <Food name='сало' />
-      <Food name='мясо' />
-      <Food name='рыбу' />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
